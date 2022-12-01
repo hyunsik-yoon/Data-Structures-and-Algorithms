8ad03b33eddcef211dc499d724914f11f6bc17b3
Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>
2022-12-01 12:33:15 -0800

    Add Chapter 73
    This adds source code and test code for chapter 73 of
    https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

---
+++ include/73-MergeSortedArrays.h
@@ -0,0 +1,8 @@
+#ifndef __73__MERGE_SORTED_ARRAYS__
+#define __73__MERGE_SORTED_ARRAYS__
+
+#include <vector>
+
+const std::vector<int> merge_sorted_arrays(const std::vector<int>& a, const std::vector<int>& b);
+
+#endif // __73__MERGE_SORTED_ARRAYS__
