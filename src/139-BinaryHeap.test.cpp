516247586cae0409444a19607860a210d3161e0f
Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>
2022-12-11 23:03:07 -0800

    Add BinaryHeap.remove_top() in chapter 139
    This adds BinaryHeap.remove_top() and test code in chapter 139.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

--- src/139-BinaryHeap.test.cpp
+++ src/139-BinaryHeap.test.cpp
@@ -62,3 +62,57 @@ TEST(binary_heap, insert_1)
     }
 }
 
+TEST(binary_heap, remove_top)
+{
+    using namespace n139;
+
+    BinaryHeap<BHType::MAX, int> binary_heap;
+    /*
+                80
+            70     50
+          30  40
+    */
+    binary_heap.insert(80);
+    binary_heap.insert(70);
+    binary_heap.insert(50);
+    binary_heap.insert(30);
+    binary_heap.insert(40);
+
+    ASSERT_EQ(binary_heap.remove_top(), 80);
+    {
+        /*
+                    70
+                40     50
+            30
+        */
+        auto actual = binary_heap.as_vector();
+        std::initializer_list<int> expected = {70, 40, 50, 30};
+        ASSERT_TRUE(std::equal(actual.begin(), actual.end(), std::vector<int>(expected).begin()));
+    }
+
+    ASSERT_EQ(binary_heap.remove_top(), 70);
+    {
+        /*
+                    50
+                40     30
+        */
+        auto actual = binary_heap.as_vector();
+        std::initializer_list<int> expected = {50, 40, 30};
+        ASSERT_TRUE(std::equal(actual.begin(), actual.end(), std::vector<int>(expected).begin()));
+    }
+
+    ASSERT_EQ(binary_heap.remove_top(), 50);
+    {
+        /*
+                    40
+                30
+        */
+        auto actual = binary_heap.as_vector();
+        std::initializer_list<int> expected = {40, 30};
+        ASSERT_TRUE(std::equal(actual.begin(), actual.end(), std::vector<int>(expected).begin()));
+    }
+
+    ASSERT_EQ(binary_heap.remove_top(), 40);
+    ASSERT_EQ(binary_heap.remove_top(), 30);
+    ASSERT_EQ(binary_heap.is_empty(), true);
+}
