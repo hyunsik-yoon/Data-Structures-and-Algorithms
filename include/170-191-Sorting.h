74170390cb5378656e4d71e61ccb5a058bb1876f
Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>
2022-12-14 16:16:36 -0800

    Add bubble sort in chapter 169
    Bubble sort and test code were added.
    
    Signed-off-by: Hyun Sik Yoon (Eric Yoon) <hyunsik.yoon.1024@gmail.com>

---
+++ include/170-191-Sorting.h
@@ -0,0 +1,14 @@
+#ifndef __170_191__SORTING__
+#define __170_191__SORTING__
+
+#include <math.h>
+#include <vector>
+
+namespace sorting
+{
+
+template <typename T> void bubble_sort(std::vector<T> &v);
+
+} // namespace sorting
+
+#endif // __170_191__SORTING__
